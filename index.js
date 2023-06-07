4bf5c6d2b644d77e04271fd478c78176949e4483
cybr-coder <codercybr@gmail.com>
2023-06-07 17:30:55 +0000

    Update JS

--- index.js
+++ index.js
@@ -1,64 +1,40 @@
-const keys = document.querySelector('.calculator-keys')
-const display = document.querySelector('.display')
-const calculator = document.querySelector('.container')
+const keys = document.querySelector(".calculator-keys");
+const display = document.querySelector(".display");
+const calculator = document.querySelector(".container");
 
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-        const key = e.target;
-        const action = key.dataset.action;
-        const keyContent = key.textContent;
-        const displayedNum = display.textContent;
-        if (!action) {
-            if (displayedNum === '0') {
-              display.textContent = keyContent
-            } else {
-                display.textContent += keyContent
-            }
-        }
-        if (action === 'decimal') {
-            display.textContent += '.';
-        }
-        
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    if (!action) {
+      if (displayedNum === "0") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent += keyContent;
+      }
     }
-})
+    if (action === "decimal") {
+      display.textContent += ".";
+    }
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      calculator.dataset.previousKeyType = "operator";
+    }
 
+    const previousKeyType = calculator.dataset.previousKeyType;
+    if (!action) {
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Basic Operation Functions
-// function add (a, b) {
-//     return a + b
-// } 
-
-// function subtract (a, b) {
-//     return a - b
-// } 
-
-// function multiply (a, b) {
-//     return a * b
-// } 
-
-// function divide (a, b) {
-//     return a / b
-// } 
